40d59497068cfd39d3aa54160724d50995140ae0
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-07-17 18:23:40 -0500

    feat: Add checkbox functionality to TodoItem component and edit task

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,10 +1,27 @@
-function TodoItem({task, onClickEdit, onClickDelete}) {
+import { useState } from 'react';
+
+function TodoItem({task, onClickEdit, onClickDelete, onClickCheckbox}) {
+    const [isEditing, setIsEditing] = useState(false);
+
+    const handlerClickEdit = (id) => {
+        setIsEditing(!isEditing)
+        onClickEdit(id)
+    }
     return (
         <li className='task'>
-            <input className='task-checkbox' name='input-check'type="checkbox" checked={task.done}/>
-            <p className='task-description'>{task.task}</p>
-            <button className='task-button edit' onClick={() => onClickEdit(task.id)}>Editar</button>
-            <button className='task-button delete'onClick={() => onClickDelete(task.id)}>Eliminar</button>
+            {isEditing ?
+                <>
+                    <input className='task-input-edit' type="text" value={task.task}/>
+                    <button className='task-save' >Guardar</button>
+                </>
+                :
+                <>
+                    <input className='task-checkbox' name='input-check'type="checkbox" checked={task.done} onChange={() => onClickCheckbox(task.id, task.done)}/>
+                    <p className='task-description'>{task.task}</p>
+                    <button className='task-button edit' onClick={() => handlerClickEdit(task.id)}>Editar</button>
+                    <button className='task-button delete'onClick={() => onClickDelete(task.id)}>Eliminar</button>
+                </>
+            }
         </li>
     );
 }
