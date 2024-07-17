b89e2bef2ab04ff8c13483231c156e47d24b0128
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-07-17 17:53:29 -0500

    chore: Refactor TaskList component to use TodoItem component

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,15 +1,12 @@
+import TodoItem from "./TodoItem.jsx";
+
 function TaskList({tasks, onClickEdit, onClickDelete }) {
     return (
         <ul>
             {tasks.length > 0 && tasks.map(
                 task => {
                     return (
-                    <li className='task' key={task.id}>
-                        <input className='task-checkbox' name='input-check'type="checkbox" checked={task.done}/>
-                        <p className='task-description'>{task.task}</p>
-                        <button className='task-button edit' onClick={() => onClickEdit(task.id)}>Editar</button>
-                        <button className='task-button delete'onClick={() => onClickDelete(task.id)}>Eliminar</button>
-                    </li>
+                        <TodoItem key={task.id} task={task} onClickEdit={onClickEdit} onClickDelete={onClickDelete} />
                     )
                 }
             )}
