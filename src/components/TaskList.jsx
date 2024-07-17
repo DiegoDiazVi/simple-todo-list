9e79045ad0b0152f6459e99bae2beec57137fd9d
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-07-17 18:23:49 -0500

    feat: Add onClickCheckbox prop to TaskList component

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,12 +1,18 @@
 import TodoItem from "./TodoItem.jsx";
 
-function TaskList({tasks, onClickEdit, onClickDelete }) {
+function TaskList({tasks, onClickEdit, onClickDelete, onClickCheckbox }) {
     return (
         <ul>
             {tasks.length > 0 && tasks.map(
                 task => {
                     return (
-                        <TodoItem key={task.id} task={task} onClickEdit={onClickEdit} onClickDelete={onClickDelete} />
+                        <TodoItem
+                            key={task.id}
+                            task={task}
+                            onClickEdit={onClickEdit}
+                            onClickDelete={onClickDelete}
+                            onClickCheckbox={onClickCheckbox}
+                        />
                     )
                 }
             )}
