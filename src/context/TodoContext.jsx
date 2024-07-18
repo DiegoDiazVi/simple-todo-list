cda22672c5eaa18df276f9276d9ee3c94d6d15fb
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-07-18 15:36:44 -0500

    feat: Refactor TodoContext to use tasksReducer for managing todo state

--- src/context/TodoContext.jsx
+++ src/context/TodoContext.jsx
@@ -1,7 +1,24 @@
+import { useReducer } from "react";
 import { createContext } from "react";
+import { tasksReducer } from '../reducers/tasksReducer.js'
 
-const TodoContext = createContext(null)
-const TodoDispatchContext = createContext(null)
+export const TodoContext = createContext(null)
+export const TodoDispatchContext = createContext(null)
 
+export default function TodoProvider({ children }) {
+    const [tasks, dispatch] = useReducer(tasksReducer, initialTasks)
+    return (
+        <TodoContext.Provider value={tasks}>
+            <TodoDispatchContext.Provider value={dispatch}>
+                { children }
+            </TodoDispatchContext.Provider>
+        </TodoContext.Provider>
+    );
+}
 
-export { TodoContext, TodoDispatchContext }
+const initialTasks = [
+    { id: 0, task: 'Levantarme', done: true },
+    { id: 1, task: 'Bañarme', done: false },
+    { id: 2, task: 'Asearme', done: false },
+    { id: 3, task: 'Desayunar', done: false },
+]
