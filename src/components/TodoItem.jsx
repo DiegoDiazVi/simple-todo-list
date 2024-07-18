605c8824cc78671b90dceeacbafc7874461efb73
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-07-18 15:07:48 -0500

    feat: Refactor TodoItem component to use TodoContext

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,5 +1,23 @@
-function TodoItem({task, onClickDelete, onClickCheckbox }) {
+import { useContext } from "react";
+import { TodoDispatchContext } from "../context/TodoContext";
 
+function TodoItem({ task }) {
+    const dispatch = useContext(TodoDispatchContext);
+
+    const handlerDelete = (id) => {
+        dispatch({
+            type: 'delete',
+            id: id
+        })
+    }
+
+    const handlerCheckBox = (id, isDone) => {
+        dispatch({
+            type: 'changedCheckBox',
+            id: id,
+            isDone: isDone
+        })
+    }
 
     return (
         <li className='task'>
@@ -8,10 +26,10 @@ function TodoItem({task, onClickDelete, onClickCheckbox }) {
                 name='input-check'
                 type="checkbox"
                 checked={task.done}
-                onChange={() => onClickCheckbox(task.id, task.done)}
+                onChange={() => handlerCheckBox(task.id, task.done)}
             />
             <p className='task-description'>{task.task}</p>
-            <button className='task-button delete'onClick={() => onClickDelete(task.id)}>Eliminar</button>
+            <button className='task-button delete'onClick={() => handlerDelete(task.id)}>Eliminar</button>
         </li>
     );
 }
