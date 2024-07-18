aa4dc077389f0cbfebdd46dc973648e22ba8d18e
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-07-17 19:11:19 -0500

    Refactor TodoItem component to simplify code and remove unnecessary editing functionality

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,34 +1,17 @@
-import { useState } from 'react';
+function TodoItem({task, onClickDelete, onClickCheckbox }) {
 
-function TodoItem({task, onClickEdit, onClickDelete, onClickCheckbox, onChangeText}) {
-    const [isEditing, setIsEditing] = useState(false);
-
-    const handlerClickEdit = (id) => {
-        setIsEditing(!isEditing)
-        onClickEdit(id)
-    }
 
     return (
         <li className='task'>
-            {isEditing ?
-                <>
-                    <input className='task-input-edit' type="text" value={task.task}/>
-                    <button className='task-save' >Guardar</button>
-                </>
-                :
-                <>
-                    <input
-                        className='task-checkbox'
-                        name='input-check'
-                        type="checkbox"
-                        checked={task.done}
-                        onChange={() => onClickCheckbox(task.id, task.done)}
-                    />
-                    <p className='task-description'>{task.task}</p>
-                    <button className='task-button edit' onClick={() => handlerClickEdit(task.id, task.task)}>Editar</button>
-                    <button className='task-button delete'onClick={() => onClickDelete(task.id)}>Eliminar</button>
-                </>
-            }
+            <input
+                className='task-checkbox'
+                name='input-check'
+                type="checkbox"
+                checked={task.done}
+                onChange={() => onClickCheckbox(task.id, task.done)}
+            />
+            <p className='task-description'>{task.task}</p>
+            <button className='task-button delete'onClick={() => onClickDelete(task.id)}>Eliminar</button>
         </li>
     );
 }
