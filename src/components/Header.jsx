5fc9c80f1a775a09157d9a8070644134218a15fc
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-07-18 15:36:29 -0500

    feat: Add form submission functionality to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,33 @@
 import { useState } from 'react'
+import { useDispatch } from '../hooks/useDispatch';
 
-function Header({ onSubmit }) {
+function Header() {
     const [text, setText] = useState('');
+    const dispatch = useDispatch()
 
     const handlerChange = (e) => {
         setText(e.target.value)
     }
 
+    const handlerSubmit = (e) => {
+        e.preventDefault();
+        let newTodo = e.target['input-task'].value
+        if (newTodo.trim() === '') return
+        dispatch({
+            type: 'add',
+            id: idNext++,
+            task: newTodo,
+            done: false
+        })
+    }
+
     return (
-        <form  className='task-form' onSubmit={onSubmit}>
+        <form  className='task-form' onSubmit={handlerSubmit}>
             <input className='task-form-input' name='input-task' type="text" placeholder='Ingresa tu tarea' value={text} onChange={handlerChange}/>
             <button className='task-form-button' type="submit"> Agregar tarea</button>
         </form>
     );
 }
 
+let idNext = 4
 export default Header;
